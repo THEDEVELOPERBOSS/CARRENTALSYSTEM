4b9844a00033066b9c560d82f99b2a2bafba98eb
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-20 19:23:19 -0600

    Save Carrental.cpp.

--- Carrental.cpp
+++ Carrental.cpp
@@ -8,5 +8,9 @@ int main(){
     
     string car1 = "Ford Raptor"; 
     string car2 = "Dodge Charger";
-	std::cout << ""
+    string car3 = "Jeep Cherokee";
+    string caroption = "default";
+    
+	std::cout << "Choose your car";
+	std::cin >> caroption;
 }
