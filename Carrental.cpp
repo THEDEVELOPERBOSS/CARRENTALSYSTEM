7784da81cd3a65a0c12a5b2d23b7dbf2d464d014
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-21 19:49:38 -0600

    Add files via upload

--- Carrental.cpp
+++ Carrental.cpp
@@ -7,13 +7,16 @@ int main() {
     string car1 = "Ford Raptor";
     string car2 = "Dodge Charger";
     string car3 = "Jeep Cherokee";
-    int caroption = 0; 
-    int newcar = 0;
+    string caroption = "default"; 
+    string newcar = "default";
+    string choice1 = "default";
+    
 
     std::cout << "Choose what you would like to do\n";
-    std::cin >> caroption || newcar; 
+    std::cin >> choice1; 
     
-    if (caroption) {
+    if (caroption == "Rent a car") {
+			
 			std::cout << "This shows that it works";
 	}
     
