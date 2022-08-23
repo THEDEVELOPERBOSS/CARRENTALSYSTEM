6195c164abab3f6c2bca0701a6ff368abbbb94b1
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-22 21:01:46 -0600

    Add files via upload

--- Carrental.cpp
+++ Carrental.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 
 int main() {
-
+	//these are the variables
     string car1 = "Ford Raptor";
     string car2 = "Dodge Charger";
     string car3 = "Jeep Cherokee";
     string caroption = "default"; 
     string newcar = "default";
     string choice1 = "default";
+    string identify = "default"
     
 
-    std::cout << "Choose what you would like to do\n";
+    std::cout << "Are you an admin or user?\n";
     std::cin >> choice1; 
     
     if ((choice1.compare(caroption) == 0) || (choice1.compare(newcar) == 0)) {
 			
 			choice1 = caroption;
-			std::cout << "This shows that it works";
+			std::cout << "This shows that it works\n";
 	}
     else {
 		choice1 = newcar;
