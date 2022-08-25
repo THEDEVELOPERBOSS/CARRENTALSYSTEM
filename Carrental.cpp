26fd701a446e9230a9edd82699e30f3f387c7b4f
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-25 17:04:34 -0600

    Add files via upload

--- Carrental.cpp
+++ Carrental.cpp
@@ -12,21 +12,27 @@ int main() {
     string choice1 = "default";
     string admin = "admin";
     string user = "user";
-    string pin1 = "Code"; 
+    string pin = "default"; 
+    string pin1 = "code";
 	//this will ask if they are an admin or a user. If they are a admin it will allow them to add cars to the program in a user friendly way. 
     std::cout << "Are you an admin or user?\n";
     std::cin >> choice1; 
 
 	if (choice1 == admin) {
 			std::cout << "Please enter your pin\n";
-			std::cin >> pin1;
+			std::cin >> pin;
 		}
 		else if (choice1 == user) {
 			std::cout << "this shows that it works\n"; 
 			}
 			
 		else{
+			std::cout << "You need to choose one of the options displayed on screen.\n";
 			
 			}
+			
+	if (pin == pin1){
+		std::cout << "this shows that it works\n";
+		}
     
 }
