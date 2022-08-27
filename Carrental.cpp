8f7dd650e54ab5e232467cece558c2cf64037e57
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2022-08-26 20:39:57 -0600

    Add files via upload

--- Carrental.cpp
+++ Carrental.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string> 
+//using std namespace will make it so yoiu can say ::cout instead of std::cout
 using namespace std;
 
 int main() {
@@ -18,28 +19,21 @@ int main() {
 	//this will ask if they are an admin or a user. If they are a admin it will allow them to add cars to the program in a user friendly way. 
     std::cout << "Are you an admin or user?\n";
     std::cin >> choice1; 
-
+//this is if they are an aadmin
 	if (choice1 == admin) {
-			std::cout << "Please enter your pin\n";
+			::cout << "Please enter your pin\n";
 			std::cin >> pin;
-		}
+		}//this is if they are a user
 		else if (choice1 == user) {
 			std::cout << "this shows that it works\n"; 
 			}
-			
+			//this is if they do not enter one of the choices 
 		else{
 			std::cout << "You need to choose one of the options displayed on screen.\n";
 			
 			}
-			
-	if (pin == pin1){
-		std::cout << "this shows that it works\n";
-		}
-    if else (pin == pin2) {
-        std::cout << "this shows that it works.\n";
-        
-    }
-    else{
-        std::cout << "either you are a hacker or your pin was entered incorrectly\n";
-    }
+			//this will ask what the the admins what they are doing
+	if (pin == pin1 || pin2) {
+		std::cout << "this shows that it works.\n";
+        }
 }
